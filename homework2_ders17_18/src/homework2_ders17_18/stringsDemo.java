378c6e8121fb48e0fa5e6d94ba2c251c8784f95e
Sevilay Bozdemir <69992495+Sevilayy21@users.noreply.github.com>
2022-09-19 20:03:48 +0300

    Update stringsDemo.java

--- homework2_ders17_18/src/homework2_ders17_18/stringsDemo.java
+++ homework2_ders17_18/src/homework2_ders17_18/stringsDemo.java
@@ -11,13 +11,13 @@ public class stringsDemo {
 		System.out.println("5. eleman : " + mesaj.charAt(4)); //index ile elemanı bulma - .charAt()
 		System.out.println(mesaj.concat(" yasasın!"));        //string ifade ekleme - .concat()
 		System.out.println(mesaj.startsWith("B"));            //baslangıcına göre true veya false dondur - .startsWith("B")
-        System.out.println(mesaj.endsWith("."));              //bitişine göre true veya false dondur - .endsWith(".")
-        char[] karakterler = new char[5];
-        mesaj.getChars(0, 5, karakterler, 0);                 //string in 0 dan 4 indexine kadar al, karakterler dizisine 0. indexinden itibaren ata
-        System.out.println(karakterler);
-        System.out.println(mesaj.indexOf("av"));              //karakterin ya da string in mesaj string de kacıncı eleman oldugunu bulmaya yarar
-        System.out.println(mesaj.lastIndexOf('a'));           //karakterin ya da string i sağdan aramaya baslar ama eleman numarası normal sayar
-        */
+                System.out.println(mesaj.endsWith("."));              //bitişine göre true veya false dondur - .endsWith(".")
+                char[] karakterler = new char[5];
+                mesaj.getChars(0, 5, karakterler, 0);                 //string in 0 dan 4 indexine kadar al, karakterler dizisine 0. indexinden itibaren ata
+                System.out.println(karakterler);
+                System.out.println(mesaj.indexOf("av"));              //karakterin ya da string in mesaj string de kacıncı eleman oldugunu bulmaya yarar
+                System.out.println(mesaj.lastIndexOf('a'));           //karakterin ya da string i sağdan aramaya baslar ama eleman numarası normal sayar
+                */
                                                       		
 		String yeniMesaj = mesaj.replace(" ", "-");           //yer değiştirme 
 		System.out.println(yeniMesaj);         
